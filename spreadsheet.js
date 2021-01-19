31e7e0e82461edcf7227842267b594b67a4f5df4
arundquist <andy.rundquist@gmail.com>
2021-01-18 20:51:30 -0600

    Update spreadsheet.js

--- spreadsheet.js
+++ spreadsheet.js
@@ -7,6 +7,25 @@ function init() {
   data["roster"].forEach(r=>roster[r[0]]={name: r[0], email:r[1], id: null});
 }
 
+function mtest() {
+  Logger.log(MD5("test"));
+}
+
+function MD5 (input) {
+  var rawHash = Utilities.computeDigest(Utilities.DigestAlgorithm.MD5, input);
+  var txtHash = '';
+  for (var i = 0; i < rawHash.length; i++) {
+    var hashVal = rawHash[i];
+    if (hashVal < 0) {
+      hashVal += 256;
+    }
+    if (hashVal.toString(16).length == 1) {
+      txtHash += '0';
+    }
+    txtHash += hashVal.toString(16);
+  }
+  return txtHash;
+}
 
 const scriptRunPromise = ()=>
 {
